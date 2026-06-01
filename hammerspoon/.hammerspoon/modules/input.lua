1b10d66a880e6bbec53e3dddad7951aaac2b1a7e
stanley <swiftwind0405@gmail.com>
2026-06-01 20:44:34 +0800

    增加输入法切换

--- hammerspoon/.hammerspoon/modules/input.lua
+++ hammerspoon/.hammerspoon/modules/input.lua
@@ -1,20 +1,23 @@
-local function Chinese()
-    hs.keycodes.currentSourceID("im.rime.inputmethod.Squirrel.Hans")
-end
+local rimeInputSourceID = "im.rime.inputmethod.Squirrel.Hans"
+local defaultEnglishInputSourceID = "com.apple.keylayout.ABC"
 
-local function English() hs.keycodes.currentSourceID("com.apple.keylayout.ABC") end
+local function Rime() hs.keycodes.currentSourceID(rimeInputSourceID) end
+
+local function DefaultEnglish() hs.keycodes.currentSourceID(defaultEnglishInputSourceID) end
 
 -- Disabled to leave Option/Meta shortcuts available for terminal/tmux.
--- hs.hotkey.bind(inputChineseHotkey, 'S', function() Chinese() end)
+-- hs.hotkey.bind(inputChineseHotkey, 'S', function() Rime() end)
 
--- hs.hotkey.bind(inputHotkey, 'E', function() English() end)
+-- hs.hotkey.bind(inputHotkey, 'E', function() DefaultEnglish() end)
 
 local function cycleInputMethod()
-    if hs.keycodes.currentSourceID() == "com.apple.keylayout.ABC" then
-        hs.keycodes.currentSourceID("im.rime.inputmethod.Squirrel.Hans")
-    elseif hs.keycodes.currentSourceID() == "im.rime.inputmethod.Squirrel.Hans" then
-        hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
+    if hs.keycodes.currentSourceID() == rimeInputSourceID then
+        DefaultEnglish()
+    else
+        Rime()
     end
 end
 
 -- hs.hotkey.bind(inputHotkey, '.', cycleInputMethod)
+-- Ctrl+Option+. avoids tmux's Option/Meta bindings and macOS Ctrl+Space handling.
+hs.hotkey.bind(inputChineseHotkey, '.', cycleInputMethod)
